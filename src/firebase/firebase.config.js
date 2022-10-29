5791376dd9499a4d5ffa911cf4f5947124d49e30
shehzad1212 <tawhidulislam261@gmail.com>
2022-10-29 18:49:28 +0600

    fire base init

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCzketaqKmrEbe1SOlpAjnfQbsAVe-k2ZE",
+  authDomain: "learning-hub-f3cfc.firebaseapp.com",
+  projectId: "learning-hub-f3cfc",
+  storageBucket: "learning-hub-f3cfc.appspot.com",
+  messagingSenderId: "384443065358",
+  appId: "1:384443065358:web:f805b9d1309d6e102ec85f",
+};
+
+// Initialize Firebase first
+const app = initializeApp(firebaseConfig);
+export default app;
